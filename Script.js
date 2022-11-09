b281d5ec4e0aedd7f0f19be560d02ee4345353ee
Iris Holmberg <holmbeir@arcada.fi>
2022-11-09 14:14:47 +0200

    Virtual PT
    Skapade en index.html samt JS skript

---
+++ Script.js
@@ -0,0 +1,16 @@
+    //Virtuell PT kod
+
+    function virtuellPT() {
+
+
+        if (minuter <= 60) {
+            alert("Dåligt, nättre kan du")
+        }
+        if (minuter >= 60 && minuter < 120) {
+            alert("Hurtigt")
+        }
+        if (minuter < 120) {
+            alert("Oj, duktigt men kom ihåg att chilla")
+        }
+    }
+    
