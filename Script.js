9a142f83df17525d595dd4fdabcad4312c99d480
Iris Holmberg <holmbeir@arcada.fi>
2022-11-11 13:27:19 +0200

    SKapade en fungerande funktion

--- Script.js
+++ Script.js
@@ -2,15 +2,19 @@
 
     function virtuellPT() {
 
+        let minuter = Number(prompt("Hur länge har du motionerat idag?"));
 
-        if (minuter <= 60) {
-            alert("Dåligt, nättre kan du")
+        if (minuter > 0 && minuter <= 60) {
+            alert("Dåligt, Bättre kan du")
         }
         if (minuter >= 60 && minuter < 120) {
             alert("Hurtigt")
         }
-        if (minuter < 120) {
+        if (minuter > 120 && minuter < 200) {
             alert("Oj, duktigt men kom ihåg att chilla")
         }
+        if (minuter == 0 || minuter > 200) {
+            alert("Jag tror inte på dig, mata in pånytt")}
+        
     }
     
